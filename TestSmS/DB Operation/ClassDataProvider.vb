e4b408c621a2ed80d42be4f7b8adcfae1f6ed4bc
taimoor <taimoorali@gmail.com>
2017-03-28 22:13:59 +0500

    ClassDataProvider

---
+++ TestSmS/DB Operation/ClassDataProvider.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.Practices.EnterpriseLibrary.Common
+Imports Microsoft.Practices.EnterpriseLibrary.Data
+
+Public Class ClassDataProvider
+    Sub New()
+
+    End Sub
+
+
+
+End Class
