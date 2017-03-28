5f1e7a8888b37a9469966a06f75187abe7933e32
taimoor <taimoorali@gmail.com>
2017-03-28 22:10:45 +0500

    update

--- TestSmS/Info/ClassInfo.vb
+++ TestSmS/Info/ClassInfo.vb
@@ -3,4 +3,13 @@
     Public Property Class_ID As Integer
     Public Property Class_Name As String
 
+    Sub New()
+
+    End Sub
+
+    Sub New(id As Integer, name As String)
+        Class_ID = id
+        Class_Name = name
+
+    End Sub
 End Class
